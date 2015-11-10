2d55f21b80c11d968e64c4ed9b649aff1a8a029a
alexqin@intv.com.cn <alexqin@intv.com.cn>
2015-11-11 03:04:15 +0800

    crawl the video url from www.zhibo8.cc by nodejs

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
+"use strict"
 var cheerio = require('cheerio'),
 	http 	= require("http"),
-	request = require('superagent')
-;
+	request = require('superagent'),
+	http = require('http'),
+	rf = require("fs")
 
 function log(){
-	var args = Array.prototype.slice.call(arguments);
-	args.unshift('[qlog] : ');
-	console.log.apply(console , args);
+	var args = Array.prototype.slice.call(arguments)
+	args.unshift('[qlog] : ')
+	console.log.apply(console , args)
 }
-request
-	.get('https://www.baidu.com')
-	.set('Accept' , 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-	.set('Accept-Encoding' , 'gzip, deflate, sdch')
-	.set('Accept-Language' , 'zh-CN,zh;q=0.8,en;q=0.6')
-	.set('Cache-Control' , 'max-age=0')
-	.set('Connection' , 'keep-alive')
-	.set('Cookie' , 'BDUSS=F5SlItLXQ5N05jOTJYdUFQNkFvelJqZ2t6ZGVSSUtVQU9zNkE2N21DeFRxbHhWQVFBQUFBJCQAAAAAAAAAAAEAAACfpz0OcWluYmluaHVhNjAxAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAFMdNVVTHTVVSG; BAIDUID=2D667C0E5F6998CE25E2D08A99793B41:FG=1; PSTM=1438883146; BIDUPSID=95A4443745A286A64C7DB5F377F70BF2; .setH_PS_645EC=e3c1MG%2FDtEbHlZYg6OavAnAJ49e6bx3DM2SgelI%2BqD12cC8ubNazfE2Uf9q7dD4lO0rThM4; BD_CK_SAM=1; BDRCVFR[ziPFLVEIFVC](=mk3SLVN4HKm; BD_HOME=1; H_PS_PSSID=14665_1458_16730_12868_16520_16800_16425_16515_15584_12101_13932_13618_16720; BD_UPN=123353')
-	.set('Host' , 'www.baidu.com')
-	.set('User-Agent' , 'Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.115 Safari/537.36')
-	.end(function(err, res){
-		data = res.text
-		$ = cheerio.load(data);
-		log($('title').text())
-	});
+
+function getData(cb) {
+	request
+		.get('http://www.zhibo8.cc/nba/luxiang.htm')
+		.set('Accept' , 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
+		.set('Accept-Encoding' , 'gzip,deflate')
+		.set('Accept-Language' , 'zh-CN')
+		.set('Cache-Control' , 'max-age=0')
+		.set('Connection' , 'keep-alive')
+		.set('Cookie' , 'bdshare_firstime=1446263591987; CNZZDATA5642869=cnzz_eid%3D376350549-1446268239-http%253A%252F%252Fwww.zhibo8.cc%252F%26ntime%3D1446268239; BAIDU_DUP_lcr=https://www.baidu.com/link?url=JST7uYIeblh_GvnOVMZUyh8Dxlrf_s8_zwV7LlkWv3W&wd=&eqid=db3350b9000023b700000003564223b1; CNZZDATA709406=cnzz_eid%3D1278675408-1446262447-null%26ntime%3D1447172836; CNZZDATA5642867=cnzz_eid%3D2032041784-1446262524-http%253A%252F%252Fwww.zhibo8.cc%252F%26ntime%3D1447173981')
+		.set('Host' , 'www.zhibo8.cc')
+		.set('User-Agent' , 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Maxthon/4.4.6.2000 Chrome/30.0.1599.101 Safari/537.36')
+		.end(function(err, res){
+			var data = res.text
+			var result = ''
+			var $ = cheerio.load(data)
+			var nodes = $('#left > .box').slice(0,3)
+			for (var i = 0; i < nodes.length; i++) {
+				var node = nodes.eq(i)
+				var base = 'http://www.zhibo8.cc/'
+				result = result + '<div class="box">' + node.html().replace(/href="\//g,'href="' + base).replace(/\|/g,'') + '</div>'
+			};
+
+			cb && cb(result)
+		})
+}
+
+function onRequest (req,res) {
+	console.log(req.url)
+	if(req.url === '/css/style.css'){
+		console.log('css request received');
+		res.writeHead(200,{"Content-Type":"text/css"})
+		rf.readFile("css/style.css",'utf-8',function(err, data) {
+			res.write(data)
+			res.end()
+		})
+	}
+	else{
+		getData(function(result){
+		    console.log('Request received');
+		    res.writeHead(200,{"Content-Type":"text/html"});
+		    res.write('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Document</title><link rel="stylesheet" href="css/style.css" /></head><body>')
+		    res.write(result);
+		    res.write('</body></html>')
+		    res.end()
+		})
+	}
+}
+
+http.createServer(onRequest).listen(8888);
+log('server started on port : 8888')
